47e19ccd6c3ce5149309793ab391e4ed65e6d06f
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-11-12 10:59:56 -0600

    Added schedule model

--- club/models.py
+++ club/models.py
@@ -20,3 +20,25 @@ class Coach(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+SCHEDULE_TYPES = (
+    ('p', 'Practice'),
+    ('n', 'No practice'),
+    ('t', 'Tournament'),
+)
+
+
+class Schedule(models.Model):
+    date = models.DateField()
+    type = models.CharField(
+        max_length=1, choices=SCHEDULE_TYPES, default='p')
+    cover_image = models.FileField(upload_to='tournaments/')
+    link = models.URLField()
+    description = models.CharField(max_length=500)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'{self.date}'
