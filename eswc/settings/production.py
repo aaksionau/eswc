e4beddfef617217fbef639f62d1efc4cbea344d4
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-11-19 15:18:24 -0600

    Static root settings for production

--- eswc/settings/production.py
+++ eswc/settings/production.py
@@ -6,7 +6,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['eswc.paloni.webfactional.com',
                  'www.eastsidewrestling.org', 'eastsidewrestling.org']
 
-STATIC_ROOT = "/static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = '/home/paloni/webapps/eswc/eswc/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
