66b68ba85e82c2587eaa9d4d40a61e4ff7783833
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-11-21 13:11:37 -0600

    Page content

--- pages/context.py
+++ pages/context.py
@@ -5,6 +5,11 @@ from pages.models import Page
 def add_menu_pages(request):
     pages = Page.objects.filter(
         published=True, visible_in_menu=True).order_by('order')
-    context = {'menu_pages': pages}
+    current_path_portions = request.path.split('/')
+    current_page = Page.objects.get(slug='home')
+    if len(current_path_portions) >= 2 and current_path_portions[1]:
+        current_page = Page.objects.filter(
+            slug=current_path_portions[1]).first()
+    context = {'menu_pages': pages, 'current_page': current_page}
 
     return context
--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ app_name = 'pages'
 
 urlpatterns = [
     path('home/', index, name='home'),
+    path('', index, name='home-default'),
     path('contacts/', FeedbackView.as_view(), name='contacts'),
     path('donate/', donate, name='donate'),
 ]
